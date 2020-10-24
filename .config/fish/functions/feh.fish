078941687f6d77b57ce04fa02e6dd28ec85f7e31
spence <spencer.d.tenney@gmail.com>
2020-10-23 22:16:14 -0400

    removed swallowing and fixed polybar fonts

--- .config/fish/functions/feh.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function feh --wraps='devour feh' --description 'alias feh devour feh'
-  devour feh $argv;
-end
--- .config/fish/functions/zathura.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function zathura --wraps='devour zathura' --description 'alias zathura devour zathura'
-  devour zathura $argv;
-end
