6ac7f825db197d4d65eb21995f4283318329e9db
spence <spencer.d.tenney@gmail.com>
2020-10-23 21:50:32 -0400

    added terminal swallowing to ranger and shell

---
+++ .config/fish/functions/feh.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function feh --wraps='devour feh' --description 'alias feh devour feh'
+  devour feh $argv;
+end
---
+++ .config/fish/functions/zathura.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function zathura --wraps='devour zathura' --description 'alias zathura devour zathura'
+  devour zathura $argv;
+end
